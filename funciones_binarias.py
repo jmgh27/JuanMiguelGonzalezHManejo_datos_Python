ab5125248e5ee5403edfe7e6ed9daf036be8be96
jmgh27 <85376330+jmgh27@users.noreply.github.com>
2021-11-22 11:59:23 -0600

    Create funciones_binarias.py

---
+++ funciones_binarias.py
@@ -0,0 +1,19 @@
+#Juan Miguel González Hernández
+import numpy as np
+
+arr = np.array([5,36,17,18,9])
+
+arr_2 = np.array([8,24,17,19,9])
+np.add(arr,arr_2)
+
+np.subtract(arr,arr_2)
+
+np.multiply(arr,arr_2)
+
+np.divide(arr,arr_2)
+
+np.array_equal(arr,arr_2)
+
+np.fmin(arr,arr_2)
+
+np.fmax(arr,arr_2)
